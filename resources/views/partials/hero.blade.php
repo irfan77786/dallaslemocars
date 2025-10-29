640db8ad75eaecc83f3f0075e160c62d39e681aa
ahmadali <nexusdeveloper09@gmail.com>
2025-10-30 00:33:43 +0500

    Text added to banner

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -3,6 +3,7 @@
         <div class="container h-100 main-hero">
             <div class="row h-100">
                 <div class="col-lg-5 col-md-8 ms-auto d-flex align-items-center search-form-internal">
+                    <h1 class="web-hero">Your Personal Chauffeur Service</h1>
                     <div class="search-section rounded p-3 mt-4">
                         @include('partials.search')
                     </div>
