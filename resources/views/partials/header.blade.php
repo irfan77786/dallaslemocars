d96641163146ea4196c07b9cfe19b7de86996d51
ahmadaliirfan <ahmadaliirfan730@gmail.com>
2025-11-22 00:00:32 +0500

    Changes done to sidebar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,8 +5,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-3 logo" style="padding-left: 8px; padding-right: 0px;">
-                        <a class="navbar-brand" style="position: relative; z-index: 9;">
-                            <img onclick="window.location.href = '{{ route('booking') }}'" src="{{ asset('assets/img/site/black-car-service-dallas-logo.webp') }}" class="logo-display" alt="shipo">
+                        <a class="navbar-brand" href="{{ url('/') }}" style="position: relative; z-index: 1;">
+                            <img src="{{ asset('assets/img/site/black-car-service-dallas-logo.webp') }}" class="logo-display" alt="shipo">
                         </a>
                     <div class="col-12">
                         <div class="responsive-menu"></div>
@@ -76,7 +76,7 @@
         <div id="drawer-overlay" class="drawer-overlay"></div>
         <aside id="mobile-drawer" class="mobile-drawer" aria-hidden="true">
             <div class="drawer-header d-flex justify-content-between align-items-center">
-                <a class="navbar-brand">
+                <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="{{ asset('assets/img/site/black-car-service-dallas-logo.webp') }}" class="logo-display" alt="shipo" style="height: 40px;">
                 </a>
                 <button class="btn btn-link p-0 drawer-close" aria-label="Close menu">
