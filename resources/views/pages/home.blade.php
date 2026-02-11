55f641043cac31748dabab04f8dedfc7aaa76bde
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-02-12 00:46:43 +0500

    align item center

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -18,14 +18,16 @@
 
             <div class="row" style="pointer-events: none;">
                 <div id="home-text-content" class="col-12 col-md-6 d-flex flex-column justify-content-center" style="pointer-events: auto; position: relative; z-index: 0;">
-                    <h1 class="text-white h1 fw-bold mb-15">Black Car Service Dallas</h1>
-                    <p class="text-white font-lg fw-medium mb-30">Lorem Ipsum is simply dummy text of the printing</p>
-                    <span class="text-white font-base">24/7 Service Available – <strong class="font-lg fw-semibold">Click to Call
-                            Now</strong></span>
-                    <p class="text-white font-base d-flex align-items-center mb-30 mb-md-0">
-                        Call: <a href="tel:+12148978056" class="mx-2 fw-bold font-lg theme-color">+1
-                            214-897-8056</a>
-                    </p>
+                    <h1 class="text-white h2 fw-bold mb-15">Black Car Service Dallas</h1>
+                    <div class="d-none d-md-block">
+                        <p class="text-white font-lg fw-medium mb-30">Lorem Ipsum is simply dummy text of the printing</p>
+                        <span class="text-white font-base">24/7 Service Available – <strong class="font-lg fw-semibold">Click to Call
+                                Now</strong></span>
+                        <p class="text-white font-base d-flex align-items-center  mb-30 mb-md-0">
+                            Call: <a href="tel:+12148978056" class="mx-2 fw-bold font-lg theme-color">+1
+                                214-897-8056</a>
+                        </p>
+                    </div>
                 </div>
                 <div class="d-none col-12 col-md-6 d-md-block" style="pointer-events: auto; position: relative; z-index: 2;">
                     <!-- Booking Form -->
@@ -40,6 +42,9 @@
             @media (min-width: 768px) {
                 #hero-banner-container {
                     min-height: 570px;
+                }
+                #home-text-content {
+                    margin-top: 130px;
                 }
                 .search-form-wrapper-desktop {
                     position: absolute;
