9e518dec4ce4df4c5d27518dbeca0669055ae10d
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-02-23 20:38:28 +0500

    hide booking summary

--- resources/views/partials/bookig-top_area.blade.php
+++ resources/views/partials/bookig-top_area.blade.php
@@ -376,7 +376,7 @@ main {
     </div>
 </div>
 
-<div class="d-md-none mb-3">
+<div class="d-md-none mb-3 d-none">
  <div class="px-3 py-2 bg-white d-flex justify-content-between align-items-center" data-bs-toggle="collapse" data-bs-target="#mobileRideSummary" aria-expanded="false" style="cursor: pointer;" onclick="toggleCollapse()">
     <h6 class="">Booking Summary</h6>
         <div class="d-flex align-items-center summary_toggle_container">
