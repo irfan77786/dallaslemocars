ce8512f948333e7db01df22fec2b2328f4ffafa5
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-05-27 02:04:18 +0500

    Revert "fix issues"
    This reverts commit 5eac4ccfd02942c8d6fced23d09ae78bf7830d95.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
         <!--  Bootstrap css plugins -->
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 
-        <link rel="stylesheet" href="{{ asset('assets/css/custom.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
