4c105e93fcc468e6be0cc2ceee1dbc541a65e0fd
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-09-01 00:58:15 +0500

    url

--- resources/views/emails/booking.blade.php
+++ resources/views/emails/booking.blade.php
@@ -17,7 +17,7 @@
 <body style="font-family: 'Abel', 'Helvetica', 'Arial', sans-serif; line-height: 1.6; color: #333333; margin: 0; padding: 0; background-color: #f4f4f4;">
     <div class="container" style="max-width: 600px; margin: 0 auto; padding: 6px; background-color: #ffffff;">
         <div class="header" style="padding: 20px 10px; text-align: center; border-bottom: 1px solid #eee;">
-            <img src="{{ config('branding.mail_logo_url') }}" alt="Dallas Black Cars" width="250" style="max-width: 250px; width: 250px; height: auto; margin-bottom: 10px; display: block; margin-left: auto; margin-right: auto; border: 0; outline: none; text-decoration: none;">
+            <img src="https://www.dallaslimoandblackcars.com/new_assets/assets/black-car-service-dallas-logo.png" alt="Dallas Black Cars" width="250" style="max-width: 250px; width: 250px; height: auto; margin-bottom: 10px; display: block; margin-left: auto; margin-right: auto; border: 0; outline: none; text-decoration: none;">
             <h2 style="margin: 0; font-size: 22px; color: #e52c43; font-weight: 600;">Booking {{ $isAdmin ? 'Notification' : 'Confirmation' }}</h2>
             <p style="margin: 5px 0 0; font-size: 15px; color: #555;">{{ $isAdmin ? 'New booking received' : 'Your reservation has been confirmed!' }}</p>
         </div>
